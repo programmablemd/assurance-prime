1baecada6954cbb684d2a43126676dd659ab3a1d
Anitha Varghese <anitha-varghese@netspective.com>
2025-12-15 15:25:03 +0530

    refactor: remove all .ts files - usage of  spry binary

--- support/assurance/qualityfolio/runbook/spry.ts
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env -S deno run -A --node-modules-dir=auto
-// Use `deno run -A --watch` in the shebang if you're contributing / developing Spry itself.
-
-import { CLI } from "../../../../bin/spry.ts";
-
-await CLI().parse(Deno.args);
